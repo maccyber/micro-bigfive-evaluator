4baf7595005e4cd78b36f70d7dd5e67421c39c00
maccyber <jonas.enge@gmail.com>
2017-02-08 17:55:58 +0100

    Added result to test

--- test/lib/score-50-test.js
+++ test/lib/score-50-test.js
@@ -8,10 +8,21 @@ const score = calculateScore(answers)
 
 tap.equal(score['E'].score, 31, 'It calculates 31 for E')
 
+tap.equal(score['E'].result, 'positive', 'It calculates positive result for E')
+
 tap.equal(score['A'].score, 30, 'It calculates 30 for A')
+
+tap.equal(score['A'].result, 'neutral', 'It calculates neutral result for A')
 
 tap.equal(score['C'].score, 31, 'It calculates 31 for C')
 
+tap.equal(score['C'].result, 'positive', 'It calculates positive result for C')
+
 tap.equal(score['N'].score, 21, 'It calculates 21 for N')
 
+tap.equal(score['N'].result, 'negative', 'It calculates negative result for N')
+
 tap.equal(score['O'].score, 33, 'It calculates 33 for O')
+
+tap.equal(score['O'].result, 'positive', 'It calculates positive result for O')
+
