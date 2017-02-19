ae0b1ad195917c4e0e6c9acbf57822150b16a3b4
maccyber <jonas.enge@gmail.com>
2017-02-19 03:28:22 +0100

    high is the new low

--- test/lib/score-50-test.js
+++ test/lib/score-50-test.js
@@ -8,7 +8,7 @@ const score = calculateScore(answers)
 
 tap.equal(score['E'].score, 31, 'It calculates 31 for E')
 
-tap.equal(score['E'].result, 'positive', 'It calculates positive result for E')
+tap.equal(score['E'].result, 'high', 'It calculates high result for E')
 
 tap.equal(score['A'].score, 30, 'It calculates 30 for A')
 
@@ -16,13 +16,13 @@ tap.equal(score['A'].result, 'neutral', 'It calculates neutral result for A')
 
 tap.equal(score['C'].score, 31, 'It calculates 31 for C')
 
-tap.equal(score['C'].result, 'positive', 'It calculates positive result for C')
+tap.equal(score['C'].result, 'high', 'It calculates high result for C')
 
 tap.equal(score['N'].score, 21, 'It calculates 21 for N')
 
-tap.equal(score['N'].result, 'negative', 'It calculates negative result for N')
+tap.equal(score['N'].result, 'low', 'It calculates low result for N')
 
 tap.equal(score['O'].score, 33, 'It calculates 33 for O')
 
-tap.equal(score['O'].result, 'positive', 'It calculates positive result for O')
+tap.equal(score['O'].result, 'high', 'It calculates high result for O')
 
